c2cd9bd0471620976d4bf558de3a3fc94aa4aff6
stacyrymer86 <36998310+stacyrymer86@users.noreply.github.com>
2022-03-05 11:42:19 +0000

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -18,6 +18,11 @@ const App = () => {
 
       if (solana && solana.isPhantom) {
           console.log('Phantom wallet found!');
+         const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
       }
