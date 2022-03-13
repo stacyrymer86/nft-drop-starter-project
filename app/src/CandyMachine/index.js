f6464d2bae0432d60ff5bb36c17248117d2fd7ea
stacyrymer86 <36998310+stacyrymer86@users.noreply.github.com>
2022-03-13 08:00:54 +0000

    comm

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect } from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
